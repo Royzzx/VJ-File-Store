4c0781db3420038005a9cfde8f7d3e30fb2c5c07
Riki <73009240+Royzzx@users.noreply.github.com>
2024-08-20 20:54:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '533890503').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "t.me/Movie_Five_File_Share_bot") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "File_store_X_bot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
