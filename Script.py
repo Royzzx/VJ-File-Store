c75cf9e92ed7d4794f4f0eae045bd6d44f24d559
Riki <73009240+Royzzx@users.noreply.github.com>
2024-08-21 11:05:00 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,11 +11,7 @@ class script(object):
 # Ask Doubt on telegram @KingVJ01
 
     
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
 
-sɪᴢᴇ ⚙️: {file_size}
-
-</b>""" 
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
